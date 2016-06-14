dfdc86925b0bec6f02c196dcca30a3382a489b3a
jydawn <1198621249@qq.com>
2016-06-14 18:17:32 +0800

    Update fibonacci_series.js

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function fibonacci_series(n) {
-
+  var temp=[0,1];
+    for(var i=2;i<=n;++i){
+        temp.push(temp[i-1] + temp[i-2]);
+    }
+    return temp;
 }
 
 module.exports = fibonacci_series;
